d9529a95a4959dc5abc1b50f07626eccad88fc6b
Felipe Rocha <felipedsousarocha@gmail.com>
2024-02-15 15:52:13 -0300

    feat: add welcome mensage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,20 @@
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { format } from "date-fns";
 
 export default function Home() {
-  return <Header></Header>;
+  return (
+    <div>
+
+      <Header></Header>
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Ola, Felipe</h2>
+        <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
+          locale: ptBR,
+        })}</p>
+      </div>
+      
+    </div>
+  )
 }
