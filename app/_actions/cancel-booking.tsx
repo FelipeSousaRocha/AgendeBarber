e64ee61164ccb8b1155658005d863139d3ed464e
Felipe Rocha <felipedsousarocha@gmail.com>
2024-03-16 22:40:56 -0300

    feat: tratment error

--- app/_actions/cancel-booking.tsx
+++ app/_actions/cancel-booking.tsx
@@ -2,14 +2,32 @@
 
 import { revalidatePath } from "next/cache";
 import { db } from "../_lib/prisma";
+import fs from "fs";
 
 export const cancelBooking = async (bookingId: string) => {
-  await db.booking.delete({
-    where: {
-      id: bookingId,
-    },
-  });
+  try {
+    // Deleta o agendamento do banco de dados
+    await db.booking.delete({
+      where: {
+        id: bookingId,
+      },
+    });
 
-  revalidatePath("/");
-  revalidatePath("/bookings");
+    // Revalida os caminhos
+    revalidatePath("/");
+    revalidatePath("/bookings");
+  } catch (error) {
+    if (error instanceof Error) {
+      // Registra o erro em um arquivo de log
+      const logMessage = `Erro ao cancelar o agendamento com ID ${bookingId}: ${error.message}\n`;
+      fs.appendFileSync("error.log", logMessage);
+
+      // Se desejar, você pode rethrow o erro para propagá-lo para cima na pilha de chamadas
+      throw error;
+    } else {
+      // Se o erro não for uma instância de Error, registre uma mensagem genérica de erro
+      const logMessage = `Erro ao cancelar o agendamento com ID ${bookingId}: erro desconhecido\n`;
+      fs.appendFileSync("error.log", logMessage);
+    }
+  }
 };
